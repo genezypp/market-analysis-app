2cfb07d0674c1267ecf6756fa4ab512ff7d9aab8
root <investbart5@gmail.com>
2024-11-27 20:47:23 +0000

    testo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import DeviceManagement from './components/DeviceManagement';
-import LoginForm from './components/LoginForm';
-import ApiTest from './components/ApiTest'; // Import testowego komponentu
-import './style.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import DeviceManagement from "./components/DeviceManagement";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
   return (
     <Router>
-      <div className="app">
+      <div className="app-container">
         <h1>Market Analysis App</h1>
-        <Switch>
-          <Route path="/" exact component={Dashboard} />
-          <Route path="/device-management" component={DeviceManagement} />
-          <Route path="/login" component={LoginForm} />
-          <Route path="/api-test" component={ApiTest} /> {/* Dodanie nowej trasy */}
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/device-management" element={<DeviceManagement />} />
+          <Route path="/login" element={<LoginForm />} />
+        </Routes>
       </div>
     </Router>
   );
